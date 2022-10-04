56f8588ca362301581b4a6098b8759d77c54dfd0
just-do-it-asap <Cfif98Cfif>
2022-10-04 23:50:37 +0300

    email sending

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const Firestore = require('@google-cloud/firestore');
-// Use your project ID here
-const PROJECTID = '***';
-const COLLECTION_NAME = '***';
+const axios = require('axios');
 
+// Use your project ID here
+const PROJECTID = 'dreamx-a4eba';
+const COLLECTION_NAME = 'cloud-functions-firestore';
+const ELASTIC_EMAIL_URL = 'https://api.elasticemail.com/v4/emails?apikey=3A79CC68F56210D3C4855D3B8E5B5FC86A598D0312BFE39CD8AE8BB3590186F59346C665C1E447BDA2C45911E9F1286E';
+
+// noinspection JSValidateTypes
 const firestore = new Firestore({
   projectId: PROJECTID,
   timestampsInSnapshots: true
@@ -11,6 +15,35 @@ const firestore = new Firestore({
   //   GOOGLE_APPLICATION_CREDENTIALS=<path>
   // keyFilename: '/cred/cloud-functions-firestore-000000000000.json',
 });
+
+const anError = (res) => {
+  res.status = 400;
+  return res.send({'status': 'error'});
+}
+
+// TODO good template
+const defaultElasticEmailBody = {
+  Recipients: [
+    {
+      Email: "",
+    }
+  ],
+  Content: {
+
+    // TODO try to change it to different emails
+    From: "alexxxxxx2019@gmail.com",
+
+    Subject: "The best email in the world",
+    TemplateName: "New Default Template:2022-07-12 00:16:23",
+
+  }
+}
+
+const sendEmail = async ({email}) => {
+  defaultElasticEmailBody.Recipients[0].Email = email;
+  console.log('defaultElasticEmailBody = ', defaultElasticEmailBody);
+  return await axios.post(ELASTIC_EMAIL_URL, defaultElasticEmailBody);
+}
 
 /**
  * Retrieve or store a method in Firestore
@@ -23,10 +56,68 @@ const firestore = new Firestore({
  * success: returns the document content in JSON format & status=200
  *    else: returns an error:<string> & status=404
  *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
+ * @param {express:Request} req HTTP request context.
+ * @param {express:Response} res HTTP response context.
  */
-exports.main = (req, res) => {
+exports.main = async (req, res) => {
+
+  // for no CORS
+  res.set('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status = 204;
+    return res.send('');
+  }
+
+  if (req.method === 'POST') {
+
+    if (!req.body) {
+      return anError(res);
+    }
+
+    let type = req.body['type'];
+
+    if (!type) {
+      return anError(res);
+    }
+
+    if (type === 'start-registration') {
+
+      let email = req.body['email'];
+
+      if (!email) {
+        return anError(res);
+      }
+
+      // TODO check that there is no user with the email.
+      // TODO possible errors:
+      // TODO 1. The verification email has already been sent
+      // TODO 2. The user with the email is already registered
+
+      // TODO create registration code\link that can be verified and add it to the email
+
+      // TODO check the result
+      console.log('email = ', email);
+      let response = await sendEmail({email});
+
+      console.log('response.data =', response.data);
+
+      res.status = 200;
+      return res.send({'status': 'success'});
+
+
+    }
+
+  }
+
+
+
+
+  // TODO remove all that is not used
+
   if (req.method === 'POST') {
     // store/insert a new document
     const data = (req.body) || {};
@@ -43,10 +134,12 @@ exports.main = (req, res) => {
       ciphertext
     }).then(doc => {
       console.info('stored new doc id#', doc.id);
-      return res.status(200).send(doc);
+      res.status = 200;
+      return res.send(doc);
     }).catch(err => {
       console.error(err);
-      return res.status(404).send({
+      res.status = 404;
+      return res.send({
         error: 'unable to store',
         err
       });
@@ -55,13 +148,15 @@ exports.main = (req, res) => {
 
   // everything below this requires an ID
   if (!(req.query && req.query.id)) {
-    return res.status(404).send({
+    res.status = 404;
+    return res.send({
       error: 'No II'
     });
   }
   const id = req.query.id.replace(/[^a-zA-Z0-9]/g, '').trim();
   if (!(id && id.length)) {
-    return res.status(404).send({
+    res.status = 404;
+    return res.send({
       error: 'Empty ID'
     });
   }
@@ -72,10 +167,12 @@ exports.main = (req, res) => {
         .doc(id)
         .delete()
         .then(() => {
-          return res.status(200).send({ status: 'ok' });
+          res.status = 200;
+          return res.send({ status: 'ok' });
         }).catch(err => {
           console.error(err);
-          return res.status(404).send({
+          res.status = 404;
+          return res.send({
             error: 'unable to delete',
             err
           });
@@ -88,20 +185,24 @@ exports.main = (req, res) => {
       .get()
       .then(doc => {
         if (!(doc && doc.exists)) {
-          return res.status(404).send({
+          res.status = 404;
+          return res.send({
             error: 'Unable to find the document'
           });
         }
         const data = doc.data();
         if (!data) {
-          return res.status(404).send({
+          res.status = 404;
+          return res.send({
             error: 'Found document is empty'
           });
         }
-        return res.status(200).send(data);
+        res.status = 200;
+        return res.send(data);
       }).catch(err => {
         console.error(err);
-        return res.status(404).send({
+        res.status = 404;
+        return res.send({
           error: 'Unable to retrieve the document',
           err
         });
